33cc2136f48973f7430d9f63b4d5c3e56c58c3f9
ethan <ethan@bowhead.com>
2023-05-09 14:42:31 +0800

    update

--- x_launch_init.py
+++ x_launch_init.py
@@ -1,8 +1,13 @@
 import os
 from xe_run_core import build_args, prepare_environment, start
 from x_scripts.init_autodl_proxy import setup_proxy_env
+from x_scripts.init_copy_fs_to_tmp import DataDirSetup
+from x_scripts.init_setup_symlink_dirs import SymlinkDirsSetup
 
 if __name__ == "__main__":
+    DataDirSetup.ensure_data_copied()
+    SymlinkDirsSetup.ensure_working_dir()
+
     os.environ["LANUCH_MODE"] = "refresh"
     setup_proxy_env()
     build_args()
--- x_launch_normal.py
+++ x_launch_normal.py
@@ -1,8 +1,10 @@
 import os
 from xe_run_core import build_args, prepare_environment, start
+from x_scripts.init_autodl_proxy import setup_proxy_env
 
 if __name__ == "__main__":
     os.environ["LANUCH_MODE"] = "normal"
+    setup_proxy_env()
     build_args()
     prepare_environment()
     start()
