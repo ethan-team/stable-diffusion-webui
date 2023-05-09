1d8525e65f778d5e85d63747ed2cb3537a96ea2d
ethan <ethan@bowhead.com>
2023-05-09 16:54:55 +0800

    update

--- x_scripts/rsync_tmp_to_fs.sh
+++ x_scripts/rsync_tmp_to_fs.sh
@@ -1,5 +1,32 @@
 #!/usr/bin/env bash
 
-rsync -av --delete /root/autodl-tmp/models/ /root/autodl-fs/sdw
+# Function to confirm the operation
+confirm_operation() {
+    read -rp "Are you sure you want to proceed? make sure you know what you are doing (y/n): " response
+    case "$response" in
+        [yY])
+            return 0
+            ;;
+        *)
+            return 1
+            ;;
+    esac
+}
 
-rsync -av --delete /root/autodl-tmp/repositories/ /root/autodl-fs/sdw
+# Example usage
+if confirm_operation; then
+    echo "Operation confirmed. Proceeding..."
+    # Perform your desired operation here
+
+    echo "Rsync models..."
+    rsync -av --delete /root/autodl-tmp/models/ /root/autodl-fs/sdw/models
+    echo ""
+
+    echo "Rsync repositories..."
+    rsync -av --delete /root/autodl-tmp/repositories/ /root/autodl-fs/sdw/repositories
+    echo ""
+
+else
+    echo "Operation canceled."
+fi
+
