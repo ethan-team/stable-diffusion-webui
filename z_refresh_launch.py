750e020325795d9b9f576e5a8dbde3c91c76c0c4
ethan <ethan@bowhead.com>
2023-05-10 11:42:38 +0800

    update

--- z_refresh_launch.py
+++ z_refresh_launch.py
@@ -3,11 +3,11 @@
 
 import os
 from xe_run_core import build_args, prepare_environment, start
-from x_scripts.init_autodl_proxy import setup_proxy_env
+#from x_scripts.init_autodl_proxy import setup_proxy_env
 
 if __name__ == "__main__":
     os.environ["LANUCH_MODE"] = "refresh::debug"
-    setup_proxy_env()
+    #setup_proxy_env()
     build_args()
     prepare_environment()
     start()
