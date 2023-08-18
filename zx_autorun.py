20584e68ecedf9370174d0610a3320827d9e4910
ethan <ethan@bh.com>
2023-08-18 21:59:46 +0800

    auto restart

--- zx_autorun.py
+++ zx_autorun.py
@@ -1,9 +1,51 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+import portalocker
+import time
 import os
+import sys
+import subprocess
+
 DIR_ROOT = os.path.dirname(__file__)
 
+LOCK_FILE = os.path.join(DIR_ROOT, 'webui.lock')
+
+def is_single_instance():
+    if os.path.exists(LOCK_FILE):
+        with open(LOCK_FILE, 'r') as f:
+            try:
+                portalocker.lock(f, portalocker.LOCK_EX | portalocker.LOCK_NB)
+                return True
+            except portalocker.LockException:
+                return False
+    else:
+        return True
+
+def main_program():
+    while True:
+        # 主程序
+        try:
+            subprocess.check_call(['python', 'zz_normal_launch.py'])
+        except subprocess.CalledProcessError as e:
+            print(e)
+            pass
+
+        print("Webui is running...")
+        time.sleep(1)
+
+
 if __name__ == "__main__":
-    print(f"{__file__} kicked @{DIR_ROOT}")
+    # print(f"{__file__} kicked @{DIR_ROOT}")
+    if is_single_instance():
+        with open(LOCK_FILE, 'w') as f:
+            portalocker.lock(f, portalocker.LOCK_EX)
+            try:
+                main_program()
+            finally:
+                os.remove(LOCK_FILE)
+    else:
+        print("Error: Program is running...")
+        sys.exit(1)
+
 
